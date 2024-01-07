8aa3d1dfcad32bc7aa7c22d57746e4916659a71c
Phan Wilkie <phanwilkie@gmail.come>
2024-01-08 09:44:11 +1300

    Game loads on pageload and image links are now working

--- javascript.js
+++ javascript.js
@@ -1,77 +1,89 @@
-//Variables
-//const playerSelection = "Rock";
-// const playerSelection = document.querySelector('.playerSubmit');
-
 //Computer randomises between rock, paper and scissors
 function getComputerChoice() {
     const computerChoices = ["Rock","Paper","Scissors"];
     const randomChoice = Math.floor(Math.random() * computerChoices.length);
-    return computerSelection = computerChoices[randomChoice].toUpperCase();
+    const computerSelection = computerChoices[randomChoice].toUpperCase();
+    return computerSelection; 
 };
 
 function playRound(playerSelection, computerSelection) {
+    let result = [];
+
     //Evaluate Computer vs Human result
-    //Output error message based on the rule
-        //where the result is tie
-        //where the computer wins
-        //where the human wins
     if (playerSelection == computerSelection) {
-        console.log("It's a tie!");
-        return roundResult = 'Tie';
-        //need to figure out how to repeat
+        console.log("It's a tie! - You: " + playerSelection + " vs Computer: " + computerSelection);
+        let roundResult = 'Tie';
+        let result = [playerSelection, computerSelection, roundResult];
+        return result;
     }
     else if (playerSelection == "ROCK") { //if human choice is rock
         if (computerSelection == "SCISSORS") {
-            console.log("Congrats! You won!");
-            return roundResult = 'Win';
+            console.log("Congrats! You won! - You: " + playerSelection + " vs Computer: " + computerSelection);
+            let roundResult = 'Win';
+            let result = [playerSelection, computerSelection, roundResult];
+            return result;
         }
         else {
-            console.log("Sorry! You've lost, better luck next time.");
-            return roundResult = 'Lost';
-        };
+            console.log("Sorry! You've lost, better luck next time. - " + playerSelection + " vs You: " + computerSelection);
+            let roundResult = 'Lost';
+            let result = [playerSelection, computerSelection, roundResult];
+            return result;
+            };
     }
     else if (playerSelection == "PAPER") { //if human choice is paper
         if (computerSelection == "ROCK") {
-            console.log("Congrats! You won!");
-            return roundResult = 'Win';
+            console.log("Congrats! You won! - You: " + playerSelection + " vs Computer: " + computerSelection);
+            let roundResult = 'Win';
+            let result = [playerSelection, computerSelection, roundResult];
+            return  result;
         }
         else {
-            console.log("Sorry! You've lost, better luck next time.");
-            return roundResult = 'Lost';
+            console.log("Sorry! You've lost, better luck next time. - " + playerSelection + " vs You: " + computerSelection);
+            let roundResult = 'Lost';
+            let result = [playerSelection, computerSelection, roundResult];
+            return result;
         };
     }
     else { //if human choice is scissors
         if (computerSelection == "PAPER") {
-            console.log("Congrats! You won!");
-            return roundResult = 'Win';
+            console.log("Congrats! You won! - You: " + playerSelection + " vs Computer: " + computerSelection);
+            let roundResult = 'Win';
+            let result = [playerSelection, computerSelection, roundResult];
+            return result;
         }
         else {
-            console.log("Sorry! You've lost, better luck next time.");
-            return roundResult = 'Lost';
+            console.log("Sorry! You've lost, better luck next time. - " + playerSelection + " vs You: " + computerSelection);
+            let roundResult = 'Lost';
+            let result = [playerSelection, computerSelection, roundResult];
+            return result;
         };
     };
 };
 
 //keep tabs of round results
+let currentRound = 1;
+const totalRounds = 5;
+
 function game() {
-    let totalRound = 1;
-    //loop until max 5 wins or 5 losts
-    for (let i = 1; i < 6; i++) {
-        let playerSelection = prompt("Rock, Paper or Scissors?").toUpperCase();
-        playRound(playerSelection, getComputerChoice());
-        console.log("Round:" + totalRound);
-        console.log("Player: " + playerSelection);
-        console.log("Computer: " + computerSelection);
-        console.log("Round Result: " + roundResult);
-        
-        //if it's a tie, then the player get to go again
-        if (roundResult == 'Tie') {
-            i--;
-        };
-        totalRound++;
-    };
 
-
+    let buttonChoice = document.querySelectorAll('.image-buttons');
+    buttonChoice.forEach(button => {
+            button.addEventListener('click', () => {
+            const buttonPlayerValue = button.getAttribute('value').toLocaleUpperCase();
+            if (currentRound <= totalRounds) {
+                const result = playRound(buttonPlayerValue, getComputerChoice());
+                // console.log('Round ' + currentRound+ " - " + result[0] + " " + result[1] +" "+ result[2]);
+                // console.log('Computer Selection ' + computerSelection)
+                currentRound++;
+                if (currentRound > totalRounds) {
+                    console.log('Game Over!')
+                }
+            }            
+            });        
+    });
 };
+            
 
-// game();
+window.onload = game;
+
+//on game over a button will appear and user can click on the button to reset the game
